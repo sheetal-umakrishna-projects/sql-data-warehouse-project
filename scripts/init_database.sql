d9808d43118e1a0f7caea23770e2972144fa8073
sheetal-umakrishna-projects <shee.umakrishna@gmail.com>
2026-03-11 18:42:15 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,22 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. 
+
+WARNING:
+    Running this script will drop the entire 'DataWarehouse' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
+
+-- Drop and recreate the 'DataWarehouse' database
+
+DROP DATABASE IF EXISTS DataWarehouse;
+
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
