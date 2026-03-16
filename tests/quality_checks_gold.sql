1057d9b5642afb2cbf5716827a0c736d9d1fb501
sheetal-umakrishna-projects <shee.umakrishna@gmail.com>
2026-03-17 02:08:19 +0530

    Update quality_checks_gold.sql

--- tests/quality_checks_gold.sql
+++ tests/quality_checks_gold.sql
@@ -1,3 +1,19 @@
+/*
+===============================================================================
+Quality Checks
+===============================================================================
+Script Purpose:
+    This script performs quality checks to validate the integrity, consistency, 
+    and accuracy of the Gold Layer. These checks ensure:
+    - Uniqueness of surrogate keys in dimension tables.
+    - Referential integrity between fact and dimension tables.
+    - Validation of relationships in the data model for analytical purposes.
+
+Usage Notes:
+    - Investigate and resolve any discrepancies found during the checks.
+===============================================================================
+*/
+
 -- Building Gold layer
 -- Here we are building the business objects like CUTOMER, PRODUCT, and SALES
 -- And then we try to connect the ones that has relationships between then
